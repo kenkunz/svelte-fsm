89719fffd1144f12dd6958f74463f7155c7822b9
Ken Kunz <kennethkunz@gmail.com>
2021-05-13 18:20:00 -0500

    subscribe function expects callback

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 export default function svelteFsm() {
+  function subscribe(callback) {
+    if (!(callback instanceof Function)) {
+      throw new TypeError('callback is not a function');
+    }
+  }
 
+  return { subscribe };
 }
--- test.js
+++ test.js
@@ -1,8 +1,35 @@
 import { assert } from 'chai';
-import fsm from './index.js';
+import svelteFsm from './index.js';
 
-describe('the module', () => {
-  it('exports a function', () => {
-    assert.isFunction(fsm);
+describe('a simple state machine', () => {
+  let simpleMachine;
+
+  beforeEach(() => {
+    simpleMachine = svelteFsm('off');
   });
+
+  it('provides a subscribe function', () => {
+    assert.isFunction(simpleMachine.subscribe);
+  });
+
+  describe('subscribe function', () => {
+    it('accepts a callback function', () => {
+      assert.doesNotThrow(() => {
+        simpleMachine.subscribe(console.log);
+      });
+    });
+
+    it('throws TypeError if no callback provided', () => {
+      assert.throws(() => {
+        simpleMachine.subscribe();
+      }, TypeError);
+    });
+
+    it('throws TypeError if callback is not a function', () => {
+      assert.throws(() => {
+        simpleMachine.subscribe('please call me back');
+      }, TypeError);
+    });
+  });
+
 });

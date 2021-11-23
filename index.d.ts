019674e33c19003f8deeecce9e335d085b253a5e
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-23 20:44:55 +0100

    add basic state typings

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,8 @@
-export default function svelteFsm(state: string | symbol, states: object): function;
+
+declare type State = string | symbol
+
+declare type States = Record<State, unknown>
+
+declare type ExtractStates<S extends States> = keyof S
+
+export default function svelteFsm<S extends States>(state: ExtractStates<S>, states: S): any;

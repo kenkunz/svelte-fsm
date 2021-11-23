d931f4c3599f02040cb8951b4c5af8110eea5044
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-23 21:27:00 +0100

    add basic state description typings

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,12 @@
 
 declare type State = string | symbol
 
-declare type States = Record<State, unknown>
+declare type States = Record<State, StateDescription>
+
+declare type StateDescription = {
+	[key: string]: State | (() => State)
+}
 
 declare type ExtractStates<S extends States> = keyof S
 
-export default function svelteFsm<S extends States>(state: ExtractStates<S>, states: S): any;
+export default function svelteFsm<S extends States>(state: ExtractStates<S>, states: S): any

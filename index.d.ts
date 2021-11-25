114de93cdaec317aac49abe8ea57eaefe00bc755
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-25 18:22:53 +0100

    add lifecycle actions typings

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,14 @@
 
 declare type State = string | symbol
 
-declare type States = Record<State, StateDescription>
+declare type States = Record<State, Actions>
 
-declare type StateDescription = {
-	[key: string]: State | ((...args: any[]) => State | void)
+declare type LifecycleAction = (argument: { from: State, to: State, event: string, args: unknown }) => void
+
+declare type Actions = {
+	_enter?: LifecycleAction
+	_exit?: LifecycleAction
+	[key: string]: State | ((...args: any[]) => State) | ((...args: any[]) => void)
 }
 
 declare type ExtractStates<S extends States> = keyof S

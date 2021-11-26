6e58d4d087f202f834fd5167566ae8f0b7eec987
Ivan Hofer <ivan.hofer@outlook.com>
2021-11-26 16:44:46 +0100

    add actions-calls typings

--- index.d.ts
+++ index.d.ts
@@ -9,18 +9,20 @@ declare type Args = any[]
 
 declare type LifecycleAction = (arg: { from: State, to: State, event: Action, args: Args }) => void
 
+declare type ActionFunction = State | ((...args: Args) => State) | ((...args: Args) => void)
+
 declare type Actions = {
 	_enter?: LifecycleAction
 	_exit?: LifecycleAction
-	[key: string]: State | ((...args: Args) => State) | ((...args: Args) => void)
+	[key: string]: ActionFunction
 }
 
 declare type ExtractStates<Sts extends States> = keyof Sts
 
 type ExtractObjectValues<A> = A[keyof A]
 
-type GetActionMapping<A extends Record<any, any>> = ExtractObjectValues<{
-	[a in keyof A]: keyof A[a]
+type GetActionMapping<A extends Record<State, Actions>> = ExtractObjectValues<{
+	[key in keyof A]: A[key] extends State ? () => A[key] : A[key] // TODO: make if-condition work
 }>
 
 declare type ExtractActions<Sts extends States> = Exclude<GetActionMapping<Sts>, '_enter' | '_exit' | number | Symbol>
@@ -29,12 +31,14 @@ declare type Unsubscribe = () => void
 
 declare type Subscribe<S> = (callback: (state: S) => void) => Unsubscribe
 
-type StateMachine<S extends State, A extends string> = {
-	[key in A]?: (...args: Args) => S // TODO: check if actions are really optional
+type StateMachine<S extends State, A> = {
+	[key in keyof A]?: A[key] // TODO: check if actions are really optional
 } & {
 	subscribe: Subscribe<S>
 }
 
-declare const svelteFsm: <Sts extends States, S extends ExtractStates<Sts>>(state: S, states: Sts) => StateMachine<ExtractStates<Sts>, ExtractActions<Sts>>
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
+
+declare const svelteFsm: <Sts extends States, S extends ExtractStates<Sts>>(state: S, states: Sts) => StateMachine<ExtractStates<Sts>, UnionToIntersection<ExtractActions<Sts>>>
 
 export default svelteFsm
